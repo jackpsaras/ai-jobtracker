9d5b66217cd3d3f68c8c2428abee39b6465203e1
Jack Psaras <jackpsaras@gmail.com>
2026-02-22 13:46:43 -0500

    changes to main.py for middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,23 @@
 # backend/app/main.py
-# make entry point for the API
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  
 from .database import engine
 from . import models
-from .routes import jobs
-from .routes import analytics
+from .routes import jobs, analytics
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="JobTrackr API")
 
-@app.get("/")
-def health_check():
-    # used to check server status
-    return {"status": "ok"}
+# Allow frontend (Vite runs on 5173 by default)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # add both just in case
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create tables.
 models.Base.metadata.create_all(bind=engine)
@@ -22,3 +27,8 @@ app.include_router(jobs.router)
 
 # Include analytics routes for dashboard
 app.include_router(analytics.router)
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
+
